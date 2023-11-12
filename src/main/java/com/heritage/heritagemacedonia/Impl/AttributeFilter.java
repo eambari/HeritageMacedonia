a8e42fe4d569e224d697ff73b52bc728a175a3c5
enluxX <endritambari@gmail.com>
2023-11-13 00:24:54 +0100

    Parse the json manually instead of importing it.

--- src/main/java/com/heritage/heritagemacedonia/Impl/AttributeFilter.java
+++ src/main/java/com/heritage/heritagemacedonia/Impl/AttributeFilter.java
@@ -1,17 +1,20 @@
 package com.heritage.heritagemacedonia.Impl;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 public class AttributeFilter implements Filter<String> {
     @Override
     public String execute(String input, String attributeName) {
-        // Parse the input JSON string
-        JsonParser parser = new JsonParser();
-        JsonObject attributes = parser.parse(input).getAsJsonObject();
-        // Check if the specified attribute is present
-        if (attributes.has(attributeName)) {
-            return attributes.get(attributeName).getAsString();
+        int attributeIndex = input.indexOf("\"" + attributeName + "\":");
+
+        // Check if the attribute is present in the JSON string
+        if (attributeIndex != -1) {
+            // Find the start and end indices of the attribute value
+            int startIndex = input.indexOf("\"", attributeIndex + attributeName.length() + 2) + 1;
+            int endIndex = input.indexOf("\"", startIndex);
+
+            // Extract the attribute value
+            if (startIndex != -1 && endIndex != -1) {
+                return input.substring(startIndex, endIndex);
+            }
         }
 
         // Return an empty string if the attribute is not present
