3416c442650fa29d611c03c32cd2cdba62da7ae2
enluxX <endritambari@gmail.com>
2023-12-02 15:15:06 +0100

    Updated the project to use maven and also added the front end directory using react.

---
+++ backend/src/test/java/com/heritage/mkheritage/MkHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heritage.mkheritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
