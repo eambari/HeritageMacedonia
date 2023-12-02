85b764b5e39481c2dc5f693f72a8ba0f20353aa0
enluxX <endritambari@gmail.com>
2023-12-02 20:45:29 +0100

    Updated the backend.

--- backend/src/main/java/com/heritage/mkheritage/MkHeritageApplication.java
+++ backend/src/main/java/com/heritage/mkheritage/MkHeritageApplication.java
@@ -1,13 +1,33 @@
 package com.heritage.mkheritage;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
 
 @SpringBootApplication
 public class MkHeritageApplication {
 
 	public static void main(String[] args) {
+		ClassLoader classLoader = MkHeritageApplication.class.getClassLoader();
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+		try (FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath())) {
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.build();
+			FirebaseApp.initializeApp(options);
+
+		} catch (Exception e) {
+			System.out.println("An error occurred while fetching the service key.");
+			System.out.println(e.getMessage());
+		}
+
 		SpringApplication.run(MkHeritageApplication.class, args);
 	}
-
 }
