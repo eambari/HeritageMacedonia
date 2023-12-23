b1dd998eccd6eac36c0a64e59bed811ccd23bef8
enluxX <endritambari@gmail.com>
2023-12-23 19:44:18 +0100

    Updated the backend.

--- src/main/java/com/heritage/mkheritage/MkHeritageApplication.java
+++ src/main/java/com/heritage/mkheritage/MkHeritageApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Objects;
 
 @SpringBootApplication
@@ -15,18 +16,23 @@ public class MkHeritageApplication {
 
 	public static void main(String[] args) {
 		ClassLoader classLoader = MkHeritageApplication.class.getClassLoader();
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey/serviceAccountKey.json")).getFile());
 
-		try (FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath())) {
-			FirebaseOptions options = FirebaseOptions.builder()
-					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.build();
-			FirebaseApp.initializeApp(options);
-
-		} catch (Exception e) {
-			System.out.println("An error occurred while fetching the service key.");
-			System.out.println(e.getMessage());
+		synchronized (MkHeritageApplication.class) {
+			if (FirebaseApp.getApps().isEmpty()) {
+				try (InputStream serviceAccount = classLoader.getResourceAsStream("serviceAccountKey/serviceAccountKey.json")) {
+					FirebaseOptions options = FirebaseOptions.builder()
+							.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+							.build();
+					FirebaseApp.initializeApp(options);
+					System.out.println("FirebaseApp initialized successfully");
+				} catch (Exception e) {
+					System.out.println("An error occurred while initializing FirebaseApp");
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+			}
 		}
+
 
 		SpringApplication.run(MkHeritageApplication.class, args);
 	}
